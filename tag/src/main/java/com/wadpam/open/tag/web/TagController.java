cba0b1f2b0f8c6c7319889781c690c77c8ddb9d3
ho <ho@goldengekko.com>
2013-03-04 17:51:08 +0700

    update TagController and add tag-service.xml

--- tag/src/main/java/com/wadpam/open/tag/web/TagController.java
+++ tag/src/main/java/com/wadpam/open/tag/web/TagController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.wadpam.docrest.domain.RestCode;
 import com.wadpam.docrest.domain.RestReturn;
@@ -32,12 +33,14 @@ public class TagController extends CrudController<JTag, DTag, Long, TagService> 
      * @return Iterable<JTag>
      */
     @RequestMapping(value = "v10/all", method = RequestMethod.GET)
+    @ResponseBody
     public Iterable<JTag> getAll() {
         return convert(service.getAll());
     }
 
     @RestReturn(value = JTag.class, code = {@RestCode(code = 200, description = "get Entity successfully", message = "ok")})
-    @RequestMapping(value = "v10/", params = "appArg0", method = RequestMethod.GET)
+    @ResponseBody
+    @RequestMapping(value = "v10", params = "appArg0", method = RequestMethod.GET)
     public Iterable<JTag> getByAppArg0(@RequestParam String appArg0) {
 
         return convert(service.getDTagByArg0(appArg0));
