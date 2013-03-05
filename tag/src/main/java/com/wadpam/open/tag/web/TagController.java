bb549132f533b1f8f22b3d84dded4afb20c585be
ho <ho@goldengekko.com>
2013-03-05 17:59:10 +0700

    add If-modified-since concept in tag service

--- tag/src/main/java/com/wadpam/open/tag/web/TagController.java
+++ tag/src/main/java/com/wadpam/open/tag/web/TagController.java
@@ -1,10 +1,19 @@
 package com.wadpam.open.tag.web;
 
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.WebRequest;
 
 import com.wadpam.docrest.domain.RestCode;
 import com.wadpam.docrest.domain.RestReturn;
@@ -25,26 +34,55 @@ import com.wadpam.open.tag.service.TagService;
 @RequestMapping(value = "{domain}/tag")
 public class TagController extends CrudController<JTag, DTag, Long, TagService> {
 
+    private final Logger LOG = LoggerFactory.getLogger(TagController.class);
+
     public TagController() {
         super(JTag.class);
     }
 
     /**
-     * @return Iterable<JTag>
+     * Retrieves all tags with support Header If-Modified-Since is optional, timestamp of last update; use
+     * "Sat, 29 Oct 1994 19:43:31 GMT"
+     * 
+     * @return Collection of JTag objects
      */
     @RequestMapping(value = "v10/all", method = RequestMethod.GET)
     @ResponseBody
-    public Iterable<JTag> getAll() {
-        return convert(service.getAll());
+    public Iterable<JTag> getAll(WebRequest webRequest, @RequestHeader(required = false, value = "If-Modified-Since") Date since,
+            HttpServletRequest request, HttpServletResponse response) {
+
+        Iterable<JTag> jTagIterable = convert(service.getAll());
+        if (since != null) {
+            final long lastUpdate = service.getLastUpdate(since);
+            if (webRequest.checkNotModified(lastUpdate)) {
+                return null;
+            }
+        }
+        return jTagIterable;
     }
 
+    /**
+     * Retrieves all tags which have reference as appArg0 with support Header If-Modified-Since is optional, timestamp of last
+     * update; use "Sat, 29 Oct 1994 19:43:31 GMT"
+     * 
+     * @param appArg0
+     *            - find all tags which have references as appArg0
+     * @return Collection of JTag objects
+     */
     @RestReturn(value = JTag.class, code = {@RestCode(code = 200, description = "get Entity successfully", message = "ok")})
     @ResponseBody
     @RequestMapping(value = "v10", params = "appArg0", method = RequestMethod.GET)
-    public Iterable<JTag> getByAppArg0(@RequestParam String appArg0) {
+    public Iterable<JTag> getByAppArg0(WebRequest webRequest,
+            @RequestHeader(required = false, value = "If-Modified-Since") Date since, @RequestParam String appArg0,
+            HttpServletRequest request, HttpServletResponse response) {
 
-        return convert(service.getDTagByArg0(appArg0));
+        Iterable<JTag> jTagIterable = convert(service.getDTagByArg0(appArg0));
 
+        if (since != null) {
+            if (webRequest.checkNotModified(service.getLastUpdate(since)))
+                return null;
+        }
+        return jTagIterable;
     }
 
     public void setService(TagService tagService) {
