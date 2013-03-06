3eac092b26842d395c60edaf6404248c672013f1
ho <ho@goldengekko.com>
2013-03-06 10:43:56 +0700

    added eclipse stuff to .gitignore

--- users/src/main/java/com/wadpam/open/user/web/OpenUserController.java
+++ users/src/main/java/com/wadpam/open/user/web/OpenUserController.java
@@ -4,13 +4,9 @@
 
 package com.wadpam.open.user.web;
 
-import com.wadpam.open.exceptions.NotFoundException;
-import com.wadpam.open.mvc.CrudController;
-import com.wadpam.open.user.domain.DOpenUser;
-import com.wadpam.open.user.json.JOpenUser;
-import com.wadpam.open.user.service.OpenUserService;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,21 +16,27 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.wadpam.open.exceptions.NotFoundException;
+import com.wadpam.open.mvc.CrudController;
+import com.wadpam.open.user.domain.DOpenUser;
+import com.wadpam.open.user.json.JOpenUser;
+import com.wadpam.open.user.service.OpenUserService;
+
 /**
- *
+ * 
  * @author sosandstrom
  */
 @Controller
 @RequestMapping("{domain}/user")
 public class OpenUserController extends CrudController<JOpenUser, DOpenUser, Long, OpenUserService> {
-    
+
     private String attributeNameUsername = "_username";
 
     public OpenUserController() {
         super(JOpenUser.class);
     }
-    
-    @RequestMapping(value="v10", method= RequestMethod.GET, params="email")
+
+    @RequestMapping(value = "v10", method = RequestMethod.GET, params = "email")
     @ResponseBody
     public JOpenUser getByEmail(@RequestParam String email) {
         DOpenUser dUser = service.getByEmail(email);
@@ -44,29 +46,34 @@ public class OpenUserController extends CrudController<JOpenUser, DOpenUser, Lon
         return convertDomain(dUser);
     }
 
-    @RequestMapping(value="v10/me", method= RequestMethod.GET)
+    @RequestMapping(value = "v10/me", method = RequestMethod.GET)
     @ResponseBody
-    public JOpenUser getMe(HttpServletRequest request,
-            HttpServletResponse response,
-            @PathVariable String domain,
-            Model model) {
+    public JOpenUser getMe(HttpServletRequest request, HttpServletResponse response, @PathVariable String domain, Model model) {
         final String username = (String) request.getAttribute(attributeNameUsername);
         final Long userId = Long.parseLong(username);
         return get(request, response, domain, userId, null, model);
     }
 
     // ----------------------- Converter and setters ---------------------------
-    
+
     @Override
     public void convertDomain(DOpenUser from, JOpenUser to) {
+        convertDOpenUser(from, to);
+    }
+
+    @Override
+    public void convertJson(JOpenUser from, DOpenUser to) {
+        convertJOpenUser(from, to);
+    }
+
+    public static void convertDOpenUser(DOpenUser from, JOpenUser to) {
         convertLongEntity(from, to);
         to.setDisplayName(from.getDisplayName());
         to.setEmail(from.getEmail());
         to.setUsername(from.getUsername());
     }
 
-    @Override
-    public void convertJson(JOpenUser from, DOpenUser to) {
+    public static void convertJOpenUser(JOpenUser from, DOpenUser to) {
         convertJLong(from, to);
         to.setDisplayName(from.getDisplayName());
         to.setEmail(from.getEmail());
